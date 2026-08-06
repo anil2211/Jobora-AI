9011c0cafecea4493c051b6c814a5eb5ba18088c
anil <anilc2812@gmail.com>
2026-08-06 21:11:38 +0530

    backend deploy on render

--- backend/src/server.js
+++ backend/src/server.js
@@ -21,7 +21,9 @@ app.get("/", (req, res) => {
   res.send("Job Saver API Running");
 });
 
-app.listen(5000, () => {
-  console.log("Server running on port 5000");
-});
+const PORT = process.env.PORT || 5000;
 
+
+app.listen(PORT,()=>{
+console.log(`Server running on ${PORT}`);
+});
